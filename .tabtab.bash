9f1c710fd0a301d7643d398f0199db48912f5a8f
Andy Henson <andrew.henson@foxsoft.net>
2008-12-15 18:11:01 +0000

    Added tabtab support and git auto completion

---
+++ .tabtab.bash
@@ -0,0 +1,5 @@
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/cucumber.rb' cucumber
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/github.rb' github
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/newgem.rb' newgem
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/rails.rb' rails
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/rubyforge.rb' rubyforge
