ccdc7b0f94b9d98df4f15287eafd0634973900e3
Andy Henson <andrew.henson@foxsoft.net>
2009-08-20 16:25:33 +0100

    Add completions for rake and config file for tabtab

--- tabtab.bash
+++ tabtab.bash
@@ -2,4 +2,5 @@ complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/cucumber.rb' 
 complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/github.rb' github
 complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/newgem.rb' newgem
 complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/rails.rb' rails
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/rake.rb' rake
 complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/rubyforge.rb' rubyforge
